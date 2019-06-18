14a7e6d76cb781ac6f73a996b4bd19f58c690a51
seanliuboyds <seanliuboyds2010@gmail.com>
2019-06-18 10:56:04 -0400

    Commit Test'

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 0; i < 10; i++){
+            for(int p = 0; p < i; p++){
+                System.out.print(i);
+            }
+            System.out.print(" \n");
+        }
+    }
+}
